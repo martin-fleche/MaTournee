142641415c7b4020b540b96134f77f1e84afdafd
Martin F <martin.flechard2021@campus-eni.fr>
2021-08-22 20:57:54 +0200

    Initial commit

---
+++ src/main/java/com/martinfleche/matournee/MaTourneeApplication.java
@@ -0,0 +1,13 @@
+package com.martinfleche.matournee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaTourneeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaTourneeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martinfleche/matournee/MaTourneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinfleche.matournee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaTourneeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
